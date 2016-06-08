53a13024f6d756ad54031599d2409e9699191bba
Max593 <maxmolotov@hotmail.it>
2016-06-08 11:26:13 +0200

    Miglioramento sostanziale su unMove di Othello

--- src/gapp/ulg/games/Othello.java
+++ src/gapp/ulg/games/Othello.java
@@ -164,12 +164,11 @@ public class Othello implements GameRuler<PieceModel<Species>> {
         if(gS.size() == 1) { return false; } //Se abbiamo appena iniziato il gioco
 
         Board<PieceModel<Species>> past = gS.get(gS.size()-2).getBoard(); //Prende la penultima board
-        for(Pos p : board.positions()) { //Per ogni posizione della board
-            if(board.get(p) != null && past.get(p) == null) { board.remove(p); } //Elimina tutti gli ADD
-            else if(board.get(p) != null && !board.get(p).equals(past.get(p))) {
-                if(past.get(p) != null) { board.put((PieceModel) past.get(p), p); } //Rigira gli SWAP
-            }
-        }
+        board.get().stream().filter(p -> !board.get(p).equals(past.get(p))).forEach(p -> { //Confronta differenze e sostituisce posizioni
+            if (past.get(p) == null) {
+                board.remove(p);
+            } else board.put(past.get(p), p);
+        });
 
         cT = gS.get(gS.size()-2).turn(); //Ritorna al turno di gioco passato (ritorna anche in gioco se necessario)
         gS.remove(gS.size()-1); //Elimina lo status su cui è stato fatto unMove
@@ -216,6 +215,22 @@ public class Othello implements GameRuler<PieceModel<Species>> {
         for(Pos p : acceptable) { //Per ogni posizione della board
             Set<Pos> swap = new HashSet<>();
             for(Board.Dir d : directions) {
+/* NON sembra velocizzare
+                if(!p.equals(new Pos(0,0)) || !p.equals(new Pos(size-1,0)) || !p.equals(new Pos(size-1,size-1)) || !p.equals(new Pos(0,size-1))) { //Angoli estremi esclusi
+                    if(p.getT() == 0 && (d.equals(Board.Dir.DOWN) || d.equals(Board.Dir.DOWN_L) ||d.equals(Board.Dir.DOWN_R))) { continue; } //Tutti i pezzi che si trovano alla base della board
+                    else if(p.getB() == 0 && (d.equals(Board.Dir.UP_L) || d.equals(Board.Dir.LEFT) || d.equals(Board.Dir.DOWN_L))) { continue; } //Posizioni sinistre
+                    else if(p.getB() == size-1 && (d.equals(Board.Dir.UP_R) || d.equals(Board.Dir.RIGHT) || d.equals(Board.Dir.DOWN_R))) { continue; } //Posizioni destre
+                    //else if(p.getT() == size-1 && (d.equals(Board.Dir.UP_L) || d.equals(Board.Dir.UP)) || d.equals(Board.Dir.UP_R)) { continue; } //Posizioni superiori [Perchè è sbagliato???]
+                }
+                if(p.equals(new Pos(0,0)) && (d.equals(Board.Dir.UP_L) || d.equals(Board.Dir.LEFT) //Angolo 0,0
+                        || d.equals(Board.Dir.DOWN_L) || d.equals(Board.Dir.DOWN) || d.equals(Board.Dir.DOWN_R))) { continue; }
+                if(p.equals(new Pos(size-1,0)) && (d.equals(Board.Dir.UP_R) || d.equals(Board.Dir.RIGHT) //Angolo inferiore destro
+                        || d.equals(Board.Dir.DOWN_R) || d.equals(Board.Dir.DOWN) || d.equals(Board.Dir.DOWN_L))) { continue; }
+                if(p.equals(new Pos(0,size-1)) && (d.equals(Board.Dir.UP_R) || d.equals(Board.Dir.UP) //Angolo superiore sinistro
+                        || d.equals(Board.Dir.UP_L) || d.equals(Board.Dir.LEFT) || d.equals(Board.Dir.DOWN_L))) { continue; }
+                if(p.equals(new Pos(size-1,size-1)) && (d.equals(Board.Dir.UP_L) || d.equals(Board.Dir.UP) //Angolo superiore destro
+                        || d.equals(Board.Dir.UP_R) || d.equals(Board.Dir.RIGHT) || d.equals(Board.Dir.DOWN_R))) { continue; }
+*/
                 try {
                     if(board.get(board.adjacent(p, d)).equals(pA)) {
                         Set<Pos> tSwap = new HashSet<>();
